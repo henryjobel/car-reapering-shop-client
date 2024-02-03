fa9ca28f29f501cebe3e251b86ffbfcb0f7fc282
Henry Joy Saha <jhobelsaha@gmail.com>
2024-02-04 04:29:32 +0600

    header created

---
+++ src/Layout/Layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Navbar from '../Shared/Navbar/Navbar';
+
+const Layout = () => {
+    return (
+        <div className='max-w-7xl mx-auto'>
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default Layout;
--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,9 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 
+import Home from "../Pages/Home/Home";
+import Layout from "../Layout/Layout";
+
+
 const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element: <Layout></Layout>,
+      children: [
+        {
+            path: "/",
+            element: <Home></Home>
+        }
+      ]
     },
   ]);
 
